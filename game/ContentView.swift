2c2acf3796a27668fd1be5ebd7bd8206512d5125
Jannnn444 <138713229+Jannnn444@users.noreply.github.com>
2024-02-18 15:04:22 +0800

    Initial Commit

---
+++ game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  game
+//
+//  Created by yucian huang on 2024/2/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ game/gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  gameApp.swift
+//  game
+//
+//  Created by yucian huang on 2024/2/18.
+//
+
+import SwiftUI
+
+@main
+struct gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
